56654ae25aa7a0f56340a0817594f82cb2f9aed7
radityadiaz11 <radityadiaz11@gmail.com>
2026-07-05 00:40:55 +0700

    Mengubah URL API ke Vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1", // tersambung ke backend v1
+    baseURL: import.meta.env.VITE_API_URL || "https://capstone-sssr.vercel.app/api/v1", // tersambung ke backend v1
     headers: {
         "Content-Type": "application/json",
     },
